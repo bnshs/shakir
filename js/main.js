7fa10e239f9c9a1c5661a2e850b6acbfd718a19f
bnshs <blag.alexand@yandex.ru>
2022-04-20 00:49:52 +0300

    Fixed dropdown-menu, used as acardion

--- js/main.js
+++ js/main.js
@@ -11,8 +11,29 @@ $('.questions__link-box').on('click', function (e){
     }
   })
 
-  $('.header__burger').on('click', function (e){
+  $('.header__burger, .overlay').on('click', function (e){
     e.preventDefault()
     $('.header__top').toggleClass('header__top--opened')
+    $('.overlay').toggleClass('overlay--show')
     $('.header__burger').toggleClass('header__burger--opened')
   })
+
+  $('.menu__list-item').on('click', function (e){
+    e.preventDefault()
+    if ($(this).hasClass('menu__list-item--active')) {
+      $(this).removeClass('menu__list-item--active')
+      $(this).children('.submenu').slideUp()
+    } else{
+      $('.menu__list-item').removeClass('menu__list-item--active')
+      $('.submenu').slideUp()
+      $(this).addClass('menu__list-item--active')
+      $(this).children('.submenu').slideDown()
+    }
+  })
+
+  // $('.header__burger, .overlay').on('click', function (e){
+  //   e.preventDefault()
+  //   $('.header__top').toggleClass('header__top--opened')
+  //   $('.overlay').toggleClass('overlay--show')
+  //   $('.header__burger').toggleClass('header__burger--opened')
+  // })
